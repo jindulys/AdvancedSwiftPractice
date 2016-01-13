acde54d05715b115bec2b8f68940616c4660ae15
yansong li <857367901@qq.com>
2016-01-12 21:17:03 -0500

    First commit

---
+++ AdvancedSwiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvancedSwiftPractice
+//
+//  Created by yansong li on 2016-01-12.
+//  Copyright © 2016 yansong li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
